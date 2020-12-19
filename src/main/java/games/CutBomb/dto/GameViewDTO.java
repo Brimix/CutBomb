7c976a7d2a0dd32d2880f0d150d429fbe2eda4a7
Brimix <brian.morris.e97@gmail.com>
2020-12-18 22:05:06 -0300

    First draft of the DTO for Game View. Needs card to have "hidden" attribute

--- src/main/java/games/CutBomb/dto/GameViewDTO.java
+++ src/main/java/games/CutBomb/dto/GameViewDTO.java
@@ -2,11 +2,34 @@ package games.CutBomb.dto;
 
 import games.CutBomb.model.GamePlay;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GameViewDTO {
-    private Map<String, Object> DTO;
+    private long id;
+    private String username;
+    private List<String> cards;
+    private List<Opponent> opponent;
 
     public GameViewDTO(GamePlay gamePlay){
+        this.id = gamePlay.getPlayer().getId();
+        this.username = gamePlay.getPlayer().getUsername();
+        this.cards = gamePlay.getCards().stream().map(card -> card.getType()).collect(Collectors.toList());
+
+    }
+}
+
+class Opponent{
+    private String username;
+    List<String> cards;
+    public Opponent(GamePlay gamePlay){
+        this.username = gamePlay.getPlayer().getUsername();
+        this.cards = new ArrayList<>();
+        this.cards = gamePlay.getCards().stream().map(card ->{
+            if(card.getHidden()) return "hidden";
+            return card.getType();
+        })
     }
 }
