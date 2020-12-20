4798ee34d788a8171d83ba6b7a782a763389ec85
Brimix <brian.morris.e97@gmail.com>
2020-12-20 16:58:45 -0300

    Added fields to show game occupancy

--- src/main/java/games/CutBomb/dto/GameDTO.java
+++ src/main/java/games/CutBomb/dto/GameDTO.java
@@ -7,12 +7,18 @@ import java.util.Date;
 public class GameDTO {
     private long id;
     private Date created;
+    private int occupancy;
+    private int capacity;
 
     public GameDTO(Game game){
         this.id = game.getId();
         this.created = game.getCreated();
+        this.occupancy = game.numberOfPlayers();
+        this.capacity = game.getCapacity();
     }
 
     public long getId() { return id; }
     public Date getCreated() { return created; }
+    public int getOccupancy() { return occupancy; }
+    public int getCapacity() { return capacity; }
 }
