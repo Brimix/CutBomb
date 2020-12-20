7ddb1ec708584e7e0946b2e490323243fe3947d7
Brimix <brian.morris.e97@gmail.com>
2020-12-20 15:06:44 -0300

    Added getter/setter for new attributes. Removed "playersNumber" which was redundant

--- src/main/java/games/CutBomb/model/Game.java
+++ src/main/java/games/CutBomb/model/Game.java
@@ -13,7 +13,6 @@ public class Game {
     @GenericGenerator(name = "native", strategy = "native")
     private long id;
     private Date created;
-    private int playersNumber;
     private int capacity;
 
     @OneToMany(mappedBy = "game", fetch = FetchType.EAGER)
@@ -25,7 +24,6 @@ public class Game {
     //~ Constructors
     public Game() {
         this.created = new Date();
-        this.playersNumber = 0;
         this.gamePlays = new HashSet<>();
         this.cards = new HashSet<>();
     }
@@ -36,8 +34,15 @@ public class Game {
 
     //~ Getters and Setters
     public long getId() { return id; }
+    public Date getCreated() { return created; }
+    public void setCreated(Date created) { this.created = created; }
+    public int getCapacity() { return capacity; }
+    public void setCapacity(int capacity) { this.capacity = capacity; }
     public Set<GamePlay> getGamePlays() { return gamePlays; }
     public void setGamePlays(Set<GamePlay> gamePlays) { this.gamePlays = gamePlays; }
     public Set<Card> getCards() { return cards; }
     public void setCards(Set<Card> cards) { this.cards = cards; }
+
+    //~ Methods
+    public int numberOfPlayers(){ return this.gamePlays.size(); }
 }
