335e6a3cd886579af6edd3d001678eb267d6db1f
Brimix <brian.morris.e97@gmail.com>
2020-12-20 21:46:15 -0300

    Created waiting-room, in which the list of players in the game is shown

---
+++ src/main/resources/static/js/waiting.js
@@ -0,0 +1,34 @@
+getPlayerList();
+
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+function getPlayerList(){
+    var url = "../api/PlayersInGame/" + getParameterByName("gp");
+    $.get(url)
+        .done(function(data){
+            var HTML = "";
+            data.forEach(function(player){
+                HTML += "<tr>";
+                HTML += "<td>" + player.id + "</td>";
+                HTML += "<td>" + player.username + "</td>";
+                HTML += "<td>" + player.joined + "</td>";
+//                HTML += "<td><button type=\"submit\""
+//                        + "class=\"form-control join-button\""
+//                        + "form=\"join-form\""
+//                        + "onclick=\"selectGame( " + game.id + ")\""
+//                        + ">Join</button></td>";
+                HTML += "</tr>";
+            });
+            document.getElementById("players-table").innerHTML = HTML;
+            console.log("players retrieved!");
+        })
+        .fail(function(){
+            console.log("couldn't retrieve players");
+        });
+}
+
+function startGame(){
+}
