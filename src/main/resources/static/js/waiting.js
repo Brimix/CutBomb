5c5e0f5f2b006f1568906b4b6af0a165cc49738b
Brimix <brian.morris.e97@gmail.com>
2021-01-02 16:41:17 -0300

    Restructured WaitingView to new version

--- src/main/resources/static/js/waiting.js
+++ src/main/resources/static/js/waiting.js
@@ -1,36 +1,73 @@
-getPlayerList();
+var waitingData = "";
+var waitingDataNew = "";
+updateView();
 
 function getParameterByName(name) {
     var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
     return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
+function updateView(){
+    loadData();
+    if(waitingData.players != waitingDataNew.players){
+        console.log(waitingDataNew);
+        waitingData = waitingDataNew;
+        processData();
+    }
+    setTimeout( function(){ updateView(); }, 1000);
+}
 
-function getPlayerList(){
-    var url = "../api/PlayersInGame/" + getParameterByName("gp");
+function loadData(){
+    var url = "api/waitingView/" + getParameterByName("gp");
     $.get(url)
         .done(function(data){
-            var HTML = "";
-            data.forEach(function(player){
-                HTML += "<tr>";
-                HTML += "<td>" + player.id + "</td>";
-                HTML += "<td>" + player.username + "</td>";
-                HTML += "<td>" + player.joined + "</td>";
-                if(player.host){
-                    HTML += "<td> HOST </td>";
-                }
-//                HTML += "<td><button type=\"submit\""
-//                        + "class=\"form-control join-button\""
-//                        + "form=\"join-form\""
-//                        + "onclick=\"selectGame( " + game.id + ")\""
-//                        + ">Join</button></td>";
-                HTML += "</tr>";
-            });
-            document.getElementById("players-table").innerHTML = HTML;
             console.log("players retrieved!");
+            waitingDataNew = data;
         })
-        .fail(function(){
+        .fail(function(data){
             console.log("couldn't retrieve players");
+            console.log(data.responseJSON.error);
         });
+}
+
+function processData(){
+    if(waitingData.started){
+        gameurl = "../game.html?gp=" + getParameterByName("gp");
+        location.href = gameurl;
+        return;
+    }
+
+    var HTML = "";
+    if(waitingData.host){
+        if(waitingData.count > 2){
+            HTML += "<center> <button "
+                    + "onclick=\"startGame()\" "
+                    + "class=\"btn btn-danger\" "
+                    + "> Start! </button> </center>"
+        }
+        else{
+            HTML += "<center><h3>Not enough players to start</h3></center>";
+        }
+    }
+    else{
+        HTML += "<center><h3>Waiting for game to start...</h3></center>"
+    }
+    document.getElementById("start-box").innerHTML = HTML;
+
+    HTML = "";
+    waitingData.players.forEach(function(player){
+        HTML += "<tr>";
+        HTML += "<td>" + player.id + "</td>";
+        HTML += "<td>" + player.username + "</td>";
+        HTML += "<td>" + player.joined + "</td>";
+        if(player.host){
+            HTML += "<td><center> HOST </center></td>";
+        }
+        else{
+            HTML += "<td></td>";
+        }
+        HTML += "</tr>";
+    });
+    document.getElementById("players-table").innerHTML = HTML;
 }
 
 function startGame(){
